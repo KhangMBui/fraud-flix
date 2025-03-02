e9bd8f56d3abf2d613f2c4f2b9045b88c2684d15
Khang Bui <khang.bui@wsu.edu>
2025-03-02 15:58:34 -0800

    Polished the home page with film images.

---
+++ src/assets/images.js
@@ -0,0 +1,35 @@
+const images = {
+  horror: [
+    "/images/Hellbound-Village.jpg",
+    "/images/Soul-Reaper.jpg",
+    "/images/The-Shining.jpg",
+    "/images/strangerThings.avif",
+    "/images/Cujo.jpg",
+    "/images/IT.jpg",
+  ],
+  action: [
+    "/images/prison-break.webp",
+    "/images/spider-man.jpg",
+    "/images/the-avengers.jpg",
+    "/images/avengers.avif",
+    "/images/Hulk.avif",
+    "/images/batman.jpg",
+  ],
+  realityShows: [
+    "/images/master-chef.webp",
+    "/images/the-great-british-bake-off.webp",
+    "/images/uncle-roger.jpg",
+    "/images/jaimie-oliver.webp",
+    "/images/next-level-chef.webp",
+    "/images/hell-kitchen.jpg",
+  ],
+  anime: [
+    "/images/dragonball-daima.jpg",
+    "/images/jujutsu-kaisen.webp",
+    "/images/sakamoto-days.webp",
+    "/images/apo-diaries.png",
+    "/images/chainsaw-man.jpg",
+  ],
+};
+
+export default images;
