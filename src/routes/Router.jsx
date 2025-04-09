0f2dfe74b56f9c6f413c5f20415e6e1628f74ea7
Khang Bui <khang.bui@wsu.edu>
2025-04-08 17:26:30 -0700

    Added search page. In progress.

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Registration from "../pages/Registration/Registration";
 import Login from "../pages/Login/Login";
+import SearchPage from "../pages/Search/SearchPage";
 
 const AppRouter = () => {
   return (
@@ -13,6 +14,9 @@ const AppRouter = () => {
         {/* Home */}
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
+
+        {/* Search page */}
+        <Route path="/Search" element={<SearchPage />} />
       </Routes>
     </Router>
   );
