93d268ea1100dff23c74315a705ef9a29b1d6ce0
Khang Bui <khang.bui@wsu.edu>
2025-04-10 00:13:48 -0700

    Refactored the backend structure in the style of the MVC model. Initialized the database.js with sequelize and the model User.js.

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./../config/database");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
