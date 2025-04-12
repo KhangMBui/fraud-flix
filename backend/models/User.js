dada74be54c76dc9f70e04353c1afbde779d75d5
aaronmhowe <aaron.howe99@gmail.com>
2025-04-11 21:38:30 -0700

    added isAdmin attribute to the User table

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,6 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    }
   });
 
   return User;
