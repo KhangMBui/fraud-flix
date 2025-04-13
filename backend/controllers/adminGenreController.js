975d0c4c1599c62fba28e822dca16342cdcc992a
aaronmhowe <aaron.howe99@gmail.com>
2025-04-13 12:37:15 -0700

    added function to admin genres controller that retrieves all content genres for the admin

--- backend/controllers/adminGenreController.js
+++ backend/controllers/adminGenreController.js
@@ -1,1 +1,18 @@
 const { Genre, Movie } = require("../models");
+
+/**
+ * Retrieves all genres.
+ * @param {*} req http request.
+ * @param {*} res http response.
+ */
+exports.getGrenres = async (req, res) => {
+  try {
+    // retrieve all genres by alphabetical order
+    const genres = await Genre.findAll({
+      sortBy: [['name', 'ASC']]
+    });
+    res.status(200).json(genres);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
