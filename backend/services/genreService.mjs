5c8bc7bb352c84ac0d7b07968890787cbb8695f5
Khang Bui <khang.bui@wsu.edu>
2025-04-12 22:39:58 -0700

    Added /services/, which has movieService and genreService scripts that fetched movies data from tmdb and put it in movies.json.

---
+++ backend/services/genreService.mjs
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+import { writeFile } from "fs/promises";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
+const API_KEY = process.env.TMDB_API_KEY;
+const API_URL = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+export async function fetchGenres() {
+  try {
+    const response = await axios.get(API_URL, options);
+    const genreMap = {};
+    response.data.genres.forEach((genre) => {
+      genreMap[genre.id] = genre.name;
+    });
+    return genreMap;
+  } catch (err) {
+    console.error(`Failed to fetch genres: `, err.message);
+    return {};
+  }
+}
+
+// async function displayGenres() {
+//   const genreMap = await fetchGenres(); // Now we're waiting for the result
+//   console.log(genreMap); // This should now log the actual genreMap
+// }
+
+// displayGenres();
