4af57b79624e0a41ae186cc96ded116e3e2803d1
aaronmhowe <aaron.howe99@gmail.com>
2025-04-12 22:59:45 -0700

    finished admin priv validation function in admin middleware

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -9,5 +9,15 @@ const SECRET = process.env.JWT_SECRET;
  * @param {*} next pass control to the next middleware function in the request-response chain.
  */
 const verifyIsAdmin = (req, res, next) => {
-
+  // initially check for a request from the user.
+  if (!req.user) {
+    return res.status(401).json({ error: "User Authentication Failed!" });
+  }
+  // then check that the user request has admin privileges.
+  if (!req.user.isAdmin) {
+    return res.status(403).json({ error: "Must Have Administrator Priveleges!" });
+  }
+  next()
 }
+
+module.exports = verifyIsAdmin;
