3173983c9586ffc30d7e0667da8300fc13c1a1b6
aaronmhowe <aaron.howe99@gmail.com>
2025-04-13 11:41:44 -0700

    added function for updating queried fields for a movie or show

--- backend/controllers/adminMovieController.js
+++ backend/controllers/adminMovieController.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const { Movie, Genre } = require("../models");
 const { Op } = require("sequelize");
 
@@ -42,5 +43,77 @@ exports.getMovies = async (req, res) => {
     });
   } catch (e) {
     res.status(404).json({ error: e.message });
+  }
+};
+
+/**
+ * Finds and retrieves a movie by ID.
+ * @param {*} req http request.
+ * @param {*} res http response.
+ */
+exports.getMovieByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findByPk(id, {
+      include: [{ model: Genre }]
+    });
+    if (!movie) {
+      return res.status(404).json({ error: "Could Not Find Requested Movie!" });
+    }
+    res.status(200).json(movie);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+/**
+ * Applies updated changes to selected content.
+ * @param {*} req http request.
+ * @param {*} res http response.
+ */
+exports.updateMovie = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      desc,
+      thumb,
+      dateReleased,
+      length,
+      director,
+      rating,
+      genreID,
+      type,
+      url
+    } = req.body;
+    // retrieve movie by id
+    const movie = await Movie.findByPk(id);
+    if (!movie) {
+      return res.status(404).json({ error: "Could Not Find Requested Movie!" });
+    }
+
+    // field updates for selected content
+    if (title !== undefined) movie.title = title;
+    if (desc !== undefined) movie.desc = desc;
+    if (thumb !== undefined) movie.thumb = thumb;
+    if (dateReleased !== undefined) movie.dateReleased = dateReleased;
+    if (length !== undefined) movie.length = length;
+    if (director !== undefined) movie.director = director;
+    if (rating !== undefined) movie.rating = rating;
+    if (genreID !== undefined) movie.genreID = genreID;
+    if (type !== undefined) movie.type = type;
+    if (url !== undefined) movie.url = url;
+
+    await movie.save();
+    // get the now updated movie/show
+    const updateMovie = await Movie.findByPk(id, {
+      include: [{ movel: Genre }]
+    });
+    res.status(200).json({
+      message: "Content Updated!",
+      movie: updateMovie
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 };
